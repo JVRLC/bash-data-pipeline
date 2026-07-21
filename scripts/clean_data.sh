f6483e98704726111687c063ac17582fe8b93259
JVRLC <ssalioub.mbaye@gmail.com>
2026-07-21 14:39:44 +0200

    add additional output messages for canceled orders and empty rows in data cleaning script

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -5,5 +5,6 @@ OUTPUT="../data/processed/clean_sales.csv"
 echo "Cleaning data..."
 
 grep -v "^$" $INPUT | grep -v "C" > $OUTPUT
-
+echo " there are $( grep "^C" $INPUT | wc -l ) canceled orders in the dataset"
+echo " thre are $( grep "^$" $INPUT | wc -l ) empty rows in the dataset"
 echo "Cleaning finished"
